bdf5d645cae71a72b999ed8fe3750db116cfc602
jujuCEEPfrontend <126881227+jujuCEEPfrontend@users.noreply.github.com>
2023-06-27 11:06:29 -0300

    Add files via upload

---
+++ TrabalhandoComAtribuiçãoDeVariáveisw.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const nome = "Julia";
+const sobrenome ="Scarpim";
+
+//console.log(nome + "" + sobrenome)
+console.log(nome, sobrenome)
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+let contador = 0
+contador = contador +1
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto =2;
