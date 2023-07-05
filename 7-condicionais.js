139339524aff0d87550e97448cbee5080ee560f3
jujuCEEPfrontend <126881227+jujuCEEPfrontend@users.noreply.github.com>
2023-07-05 09:03:44 -0300

    Add files via upload

---
+++ 7-condicionais.js
@@ -0,0 +1,34 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`
+    `Sâo Paulo`
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 17;
+
+
+
+
+console.log("Destinos possíveis");
+console.log(listaDeDestinos);
+
+//if(idadeComprador >= 18){
+//    console.log("Comprador maior de idade");
+//   listaDeDestinos.splice(1,1); //removendo item
+//}else if(estaAcompanhada ==true){
+//        console.log("Comprador está acompanhado");
+//        listaDeDestinos.splice(1,1); //removendo item
+//}else{
+//        console.log("Não é maior de Idade e não posso vender");
+//    }
+
+
+
+console.log(listaDeDestinos);
+
+console.log(idadeComprador > 18);
+console.log(idadeComprador < 18);
+console.log(idadeComprador <= 18);
+console.log(idadeComprador >= 18);
+console.log(idadeComprador == 18);
